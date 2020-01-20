ab2323c818126e5387aeb69abe58335de88ae237
Abdi <oxabdi75@gmail.com>
2020-01-20 09:31:02 +0300

    added css and background image

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -54,8 +54,5 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit =SubmitField('Post')
     
-class CommentForm(FlaskForm):
-    
-    content = TextAreaField('Content')
-    submit =SubmitField('Post')
+
     
